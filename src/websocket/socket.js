04f2f988b68d826a9a86f897ce15a42e71e1087d
leonardo-fabricio <leonnardofabbricio@gmail.com>
2025-09-23 22:22:46 -0300

    feat: verify client token for connection on web socket

--- src/websocket/socket.js
+++ src/websocket/socket.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const WebSocket = require("ws");
 
+const SOCKET_KEY = process.env.SOCKET_KEY;
 const PORT = process.env.PORT;
 const clients = new Set();
 let wss;
@@ -10,7 +11,20 @@ const initializeWebSocketServer = (expressServer) => {
     return;
   }
 
-  wss = new WebSocket.Server({ server: expressServer });
+  wss = new WebSocket.Server({
+    server: expressServer,
+    verifyClient: (info, done) => {
+      const url = new URL(info.req.url, `http://${info.req.headers.host}`);
+      const token = url.searchParams.get("token");
+
+      if (token && token === SOCKET_KEY) {
+        return done(true);
+      } else {
+        console.log("WebSocket connection rejected: Invalid or missing token.");
+        return done(false, 401, "Unauthorized");
+      }
+    },
+  });
 
   wss.on("connection", (ws) => {
     console.log("Client connected via WebSocket.");
